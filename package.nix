822896536a43a9a412f0ed066c27fe200dea49c0
Vaios K <jjacke@proton.me>
2026-03-29 12:36:53 +0300

    update to v0.2.4 no matrix support because it requires olm package which is listed as insecure in nixpkgs

--- package.nix
+++ package.nix
@@ -6,21 +6,32 @@
 
 buildGoModule rec {
   pname = "picoclaw";
-  version = "0.2.3";
+  version = "0.2.4";
 
   src = fetchFromGitHub {
     owner = "sipeed";
     repo = "picoclaw";
     rev = "v${version}";
-    hash = "sha256-CnwfnYl7hciCbgC0P/I9anGdmrzpRalutGmPAJ6H7NI=";
+    hash = "sha256-CSAUlCe/g22rzbOx3xNTMFRIOwp/+ezCMRCjNRcQeZ0=";
   };
 
-  vendorHash = "sha256-3MjBLklUpMTcz5/tW7Lr6d4wJ1x7ylFiEZkyeJI0CUA=";
+  vendorHash = "sha256-ocLRiFZs2OnKM7C2/cUafpC8LIRCLybXG0ln8n9ZXr4=";
 
+  # Relax Go version requirement (upstream requires 1.25.8, nixpkgs has 1.25.7)
   # Copy workspace for go:embed directive (moved to onboard subpackage in v0.2.0)
   postPatch = ''
+    sed -i 's/go 1.25.8/go 1.25.7/' go.mod
     cp -r workspace cmd/picoclaw/internal/onboard/
+    # Remove Matrix channel (requires libolm which is insecure in nixpkgs)
+    rm pkg/gateway/channel_matrix.go
   '';
+
+  overrideModAttrs = _: {
+    env.GOFLAGS = "-mod=mod";
+  };
+
+  # Static build without CGo (disables Matrix/libolm which is insecure in nixpkgs)
+  env.CGO_ENABLED = 0;
 
   subPackages = [ "cmd/picoclaw" ];
   doCheck = false;
